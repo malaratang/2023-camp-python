1e20237c1156449c13b073e38d613db9b649fca6
malaratang <141621323+malaratang@users.noreply.github.com>
2023-08-08 18:03:35 +0900

    Add files via upload

---
+++ 2일차/배고프다.py
@@ -0,0 +1,70 @@
+# R=int(8)
+# print(R)
+# A=float(88)
+# print(A)
+# B=bool(920)
+# print(B)
+# C=str("핑크퐁")
+# print(C)
+# print('''뽀로롱
+#       우와 뽀로로다''')
+# print("우와\n뽀로로다")
+# E=str("천사님기다리고있어요")
+# print(E[0:3])
+# print(len(E))
+
+# F=45
+# F=F+1
+# print(F)
+# G=48
+# G+=12345
+# print(G)
+# X=10
+# Y=10
+# X+=1
+# Y*=2
+# print(f"X={X},Y={Y}")
+# r=3
+# p=3.14
+# print(r*r*p)
+# x=int(input())
+# y=int(input())
+# print(f"x==y의 결과값: {x==y}")
+# print(f"x!=y의 결과값: {x!=y}")
+# print(f"x>y의 결과값: {x>y}")
+# print(f"x<y의 결과값: {x<y}")
+# print(f"x>=y의 결과값: {x>=y}")
+# print(f"x<=y의 결과값: {x<=y}")
+# n=int(input())
+# print(n%4==0 & n%100!=0 & n%400==0)
+
+# import turtle
+# a=turtle.Pen()
+# a.shape('turtle')
+# a.color('sky blue')
+# a.begin_fill()
+# a.circle(130)
+# a.end_fill()
+
+# turtle.exitonclick()
+
+import turtle
+a=turtle.Pen()
+a.color('sky blue')
+a.shape('triangle')
+b=turtle.Pen()
+b.color('pink')
+b.shape('circle')
+a.forward(200)
+b.forward(100)
+b.left(90)
+b.forward(70)
+b.right(90)
+b.forward(100)
+
+turtle.exitonclick()
+
+import turtle
+a=turtle.Pen()
+a.color('rad')
+a.shape('triangle')
---
+++ 2일차/이름.py
@@ -0,0 +1,28 @@
+# password=1234
+# masterpw=0000
+# num=int(input('비밀번호를 입력해주세요:'))
+# if password==num:
+#     print('문이 열렸습니다')
+# elif masterpw==num:
+#     print('문이 열렸습니다')
+# else:
+#     print('삐용요요ㅛ오오오옹ㅗㅗㅗ')
+
+# h= int(input())
+# if h>140:
+#     print('탑승할 수 있습니다')
+# else:
+#     print('탑승할 수 없습니다')
+
+# i=0
+# while i<=10:
+#     print('반복문이 돌아갑니다')
+#     i+=1
+
+# for i in range(10):
+#     print('반복되는 일상')
+
+# i=0
+# while True:
+#     print(i, end=' ')
+#     i+=1
